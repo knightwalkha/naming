2305a2c135163dc2b20701e4e728a38131523f97
knightwalkha <ohiosumuankehinde@gmail.com>
2022-06-23 20:33:46 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var firstName = "Kehinde"
+var lastName = "Ohiosumuan"
+var height = 180
+var country = "Nigeria"
+
+alert(firstName + lastName + " is " + height + "cm tall and resides in " + country)
